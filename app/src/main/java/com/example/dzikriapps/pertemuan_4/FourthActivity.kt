9a762e58d925f6bca62fa99b01b3ba74ca5369f1
dzikripcr <dzikri24si@mahasiswa.pcr.ac.id>
2026-04-06 10:22:44 +0700

    penambahan snackbox, alert dialog dan card view - P4

--- app/src/main/java/com/example/dzikriapps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/dzikriapps/pertemuan_4/FourthActivity.kt
@@ -11,6 +11,8 @@ import com.example.dzikriapps.MainActivity
 import com.example.dzikriapps.R
 import com.example.dzikriapps.databinding.ActivityFourthBinding
 import com.example.dzikriapps.databinding.ActivityThirdBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 
 class FourthActivity : AppCompatActivity() {
@@ -32,6 +34,29 @@ class FourthActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Tutup"){
+                    Log.e("Info Snackbar","Snackbar ditutup")
+                }
+                .show()
+        }
+
+        binding.btnShowAlertDialog.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
         }
 
         val name = intent.getStringExtra("name")
