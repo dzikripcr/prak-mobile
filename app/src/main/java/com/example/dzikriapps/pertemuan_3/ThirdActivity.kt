22210007cd6f170bba9457d6af516dffa0c92d5a
dzikripcr <dzikri24si@mahasiswa.pcr.ac.id>
2026-04-06 08:04:43 +0700

    mengirim data antar activity - P4

--- app/src/main/java/com/example/dzikriapps/pertemuan_3/ThirdActivity.kt
+++ app/src/main/java/com/example/dzikriapps/pertemuan_3/ThirdActivity.kt
@@ -2,8 +2,6 @@ package com.example.dzikriapps.pertemuan_3
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
---
+++ app/src/main/java/com/example/dzikriapps/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,42 @@
+package com.example.dzikriapps.pertemuan_4
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dzikriapps.MainActivity
+import com.example.dzikriapps.R
+import com.example.dzikriapps.databinding.ActivityFourthBinding
+import com.example.dzikriapps.databinding.ActivityThirdBinding
+
+
+class FourthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFourthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityFourthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age",0)
+
+        Log.e("Data Intent == ","Nama: $name , Usia: $age, Asal: $from")
+    }
+}
