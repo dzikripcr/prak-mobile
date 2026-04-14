c03e648142b63832966f7a3efdaf676edab49201
dzikripcr <dzikri24si@mahasisa.pcr.ac.id>
2026-04-14 14:40:49 +0700

    Penambahan Option Menu - P5

--- app/src/main/java/com/example/dzikriapps/pertemuan_5/FifthActivity.kt
+++ app/src/main/java/com/example/dzikriapps/pertemuan_5/FifthActivity.kt
@@ -1,7 +1,9 @@
 package com.example.dzikriapps.pertemuan_5
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,14 +26,19 @@ class FifthActivity : AppCompatActivity() {
             insets
         }
 
-//        setSupportActionBar(binding.toolbar)
-//        supportActionBar?.apply {
-//            title = "Activity Fifth"
-//            subtitle = "Ini adalah subtitle"
-//            setDisplayHomeAsUpEnabled(true)
-//            setDisplayShowHomeEnabled(true)
-//        }
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            title = "Activity Fifth"
+            subtitle = "Ini adalah subtitle"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
     }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -39,8 +46,17 @@ class FifthActivity : AppCompatActivity() {
                 onBackPressedDispatcher.onBackPressed()
                 true
             }
-
+            R.id.action_search -> {
+                Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+
     }
+
 }
