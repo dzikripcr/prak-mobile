519e8a405e1966adb02e23ebafeb5c80d64b5c7b
dzikripcr <dzikri24si@mahasisa.pcr.ac.id>
2026-05-04 10:08:14 +0700

    Membuat ListView (Custom Adapter) - P8

--- app/src/main/java/com/example/dzikriapps/Message/MessageFragment.kt
+++ app/src/main/java/com/example/dzikriapps/Message/MessageFragment.kt
@@ -13,6 +13,19 @@ class MessageFragment : Fragment() {
     private var _binding: FragmentMessageBinding? = null
     private val binding get() = _binding!!
 
+    private val messageList = listOf(
+        MessageModel("Alya", "Halo! Apa kabar?", "https://i.pravatar.cc/150?img=1"),
+        MessageModel("Budi", "Sudah makan?", "https://i.pravatar.cc/150?img=2"),
+        MessageModel("Citra", "Jangan lupa tugasnya ya!", "https://i.pravatar.cc/150?img=3"),
+        MessageModel("Dika", "Besok kita rapat jam 9", "https://i.pravatar.cc/150?img=4"),
+        MessageModel("Eka", "Nice job kemarin!", "https://i.pravatar.cc/150?img=5"),
+        MessageModel("Fajar", "Lagi ngapain?", "https://i.pravatar.cc/150?img=6"),
+        MessageModel("Gita", "Boleh minta tolong?", "https://i.pravatar.cc/150?img=7"),
+        MessageModel("Hana", "Lihat email ya", "https://i.pravatar.cc/150?img=8"),
+        MessageModel("Irfan", "Oke noted", "https://i.pravatar.cc/150?img=9"),
+        MessageModel("Joko", "Sampai jumpa besok", "https://i.pravatar.cc/150?img=10")
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,6 +46,9 @@ class MessageFragment : Fragment() {
         (requireActivity() as AppCompatActivity).supportActionBar?.apply {
             title = "Message"
         }
+
+        val adapter = MessageAdapter(requireContext(), messageList)
+        binding.listMessageItems.adapter = adapter
     }
 
 }
